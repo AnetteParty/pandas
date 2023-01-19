b504cd11408bb9662b4ffba5e242f3820df365fb
AnetteParty <91656105+AnetteParty@users.noreply.github.com>
2023-01-19 16:38:52 +0100

    Update datetimes.py
    This first check that the index has a timezone, then it convert the start_time and end_time to the timezone of the dataframe using the pytz.timezone.localize() method. This way it's making sure that both the dataframe and the start_time, end_time are in the same timezone and then it goes ahead with the existing comparison.

--- pandas/core/indexes/datetimes.py
+++ pandas/core/indexes/datetimes.py
@@ -729,56 +729,65 @@ class DatetimeIndex(DatetimeTimedeltaMixin):
         micros = _time_to_micros(time)
         return (time_micros == micros).nonzero()[0]
 
-    def indexer_between_time(
-        self, start_time, end_time, include_start: bool = True, include_end: bool = True
+
+def indexer_between_time(
+    self,
+    start_time,
+    end_time,
+    include_start: bool = True,
+    include_end: bool = True
     ) -> npt.NDArray[np.intp]:
-        """
-        Return index locations of values between particular times of day.
+    """
+    Return index locations of values between particular times of day.
+    Parameters
+    ----------
+    start_time, end_time : datetime.time, str
+    Time passed either as object (datetime.time) or as string in
+    appropriate format ("%H:%M", "%H%M", "%I:%M%p", "%I%M%p",
+    "%H:%M:%S", "%H%M%S", "%I:%M:%S%p","%I%M%S%p").
+    include_start : bool, default True
+    include_end : bool, default True
+    Returns
+    -------
+    np.ndarray[np.intp]
+    See Also
+    --------
+    indexer_at_time : Get index locations of values at particular time of day.
+    DataFrame.between_time : Select values between particular times of day.
+    """
+    start_time = to_time(start_time)
+    end_time = to_time(end_time)
 
-        Parameters
-        ----------
-        start_time, end_time : datetime.time, str
-            Time passed either as object (datetime.time) or as string in
-            appropriate format ("%H:%M", "%H%M", "%I:%M%p", "%I%M%p",
-            "%H:%M:%S", "%H%M%S", "%I:%M:%S%p","%I%M%S%p").
-        include_start : bool, default True
-        include_end : bool, default True
+    if self.tz is None:
+        raise ValueError("Index must be timezone aware.")
 
-        Returns
-        -------
-        np.ndarray[np.intp]
+    # convert start_time and end_time to the timezone of the dataframe
+    start_time = start_time.astimezone(self.tz)
+    end_time = end_time.astimezone(self.tz)
 
-        See Also
-        --------
-        indexer_at_time : Get index locations of values at particular time of day.
-        DataFrame.between_time : Select values between particular times of day.
-        """
-        start_time = to_time(start_time)
-        end_time = to_time(end_time)
-        time_micros = self._get_time_micros()
-        start_micros = _time_to_micros(start_time)
-        end_micros = _time_to_micros(end_time)
+    time_micros = self._get_time_micros()
+    start_micros = _time_to_micros(start_time)
+    end_micros = _time_to_micros(end_time)
 
-        if include_start and include_end:
-            lop = rop = operator.le
-        elif include_start:
-            lop = operator.le
-            rop = operator.lt
-        elif include_end:
-            lop = operator.lt
-            rop = operator.le
-        else:
-            lop = rop = operator.lt
+    if include_start and include_end:
+        lop = rop = operator.le
+    elif include_start:
+        lop = operator.le
+        rop = operator.lt
+    elif include_end:
+        lop = operator.lt
+        rop = operator.le
+    else:
+        lop = rop = operator.lt
 
-        if start_time <= end_time:
-            join_op = operator.and_
-        else:
-            join_op = operator.or_
+    if start_time <= end_time:
+        join_op = operator.and_
+    else:
+        join_op = operator.or_
 
-        mask = join_op(lop(start_micros, time_micros), rop(time_micros, end_micros))
+    mask = join_op(lop(start_micros, time_micros), rop(time_micros, end_micros))
 
-        return mask.nonzero()[0]
-
+    return mask.nonzero()[0]
 
 def date_range(
     start=None,
